9c7b67bda643d06e83f061b771039970847493a5
Markus Remy <44133097+Preunicom@users.noreply.github.com>
2026-03-07 17:29:57 +0100

    Update script to use Vitis instead of Vivado

---
+++ scripts/start_vitis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script is run whenever the desktop environment has started.
+# (with normal user privileges).
+
+script_dir=$(dirname -- "$(readlink -nf $0)";)
+source "$script_dir/header.sh"
+validate_linux
+
+# Change working directory to a writable folder to allow Vivado to write its logs.
+cd /home/user/XilinxLogs
+
+export LD_PRELOAD="/lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libselinux.so.1 /lib/x86_64-linux-gnu/libz.so.1 /lib/x86_64-linux-gnu/libgdk-x11-2.0.so.0"
+
+# if Vivado is installed
+if [ -d "/home/user/Xilinx" ]
+then
+	# Make Vivado connect to the xvcd server running on macOS (the version number can be above and below the Vivado folder level depending on the Vivado version)
+	if  [ -d "/home/user/Xilinx/Vivado" ]
+	then
+		/home/user/Xilinx/Vitis/*/settings64.sh
+		/home/user/Xilinx/Vitis/*/bin/vitis
+	else
+		/home/user/Xilinx/*/Vitis/settings64.sh
+		/home/user/Xilinx/*/Vitis/bin/vitis
+	fi
+else
+	f_echo "The installation is incomplete."
+	wait_for_user_input
+fi
