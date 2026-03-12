3d063b38ede0360fd26e311c91f7d4870ba44308
Preunicom <44133097+Preunicom@users.noreply.github.com>
2026-03-12 10:24:47 +0100

    Fixed component creation error with shared libraries by removing preloaded libs

--- scripts/start_vitis.sh
+++ scripts/start_vitis.sh
@@ -10,8 +10,6 @@ validate_linux
 # Change working directory to a writable folder to allow Vivado to write its logs.
 cd /home/user/XilinxLogs
 
-export LD_PRELOAD="/lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libselinux.so.1 /lib/x86_64-linux-gnu/libz.so.1"
-
 # if Vivado is installed
 if [ -d "/home/user/Xilinx" ]
 then
