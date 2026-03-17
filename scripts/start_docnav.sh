2be78e7afbc2693b30445644fc5ab881434e1994
Preunicom <44133097+Preunicom@users.noreply.github.com>
2026-03-17 09:35:57 +0100

    Added DocNav to desktop

---
+++ scripts/start_docnav.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is run whenever the desktop environment has started or the desktop entry is clicked.
+# (with normal user privileges).
+
+script_dir="/home/user/scripts"
+source "$script_dir/header.sh"
+validate_linux
+
+# Change working directory to a writable folder.
+cd /home/user/XilinxLogs
+
+# if Vivado is installed
+if [ -d "/home/user/Xilinx" ]
+then
+	/home/user/Xilinx/DocNav/docnav
+	f_echo "DocNav opened! Do not close this window!"
+    wait_for_user_input
+else
+	f_echo "The installation is incomplete."
+	wait_for_user_input
+fi
